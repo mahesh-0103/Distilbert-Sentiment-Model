8fd3333048d6a92bfbb8fcdecd2a6f632bc5487a
Maheswaran S <mahesh.capt7@gmail.com>
2026-01-25 11:44:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-# Start FastAPI in the background
-uvicorn api.app:app --host 0.0.0.0 --port 8000 &
 
-# Wait 15 seconds for the model to load
-sleep 15
-
-# Start Streamlit
-streamlit run streamlit_app.py --server.port $PORT --server.address 0.0.0.0
